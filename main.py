b16fec18e4f78c2f3cd747c805e305083020a08b
Luniana <80320442+Luniana@users.noreply.github.com>
2023-01-20 16:33:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ reddit = praw.Reddit(
     ratelimit_seconds=700
 )
 
-subreddits = [reddit.subreddit("memes"), reddit.subreddit("shitposting"),
-              reddit.subreddit("196"), reddit.subreddit("dankmemes"), reddit.subreddit("danidev"),
+subreddits = [reddit.subreddit("memes"),
+              # reddit.subreddit("shitposting"),
+              # reddit.subreddit("196"),
+              reddit.subreddit("dankmemes"), reddit.subreddit("danidev"),
               # reddit.subreddit("copypasta"),
               reddit.subreddit("guro"),
               # reddit.subreddit("whitepeopletwitter"),
@@ -37,18 +39,27 @@ def blablabla(comment):
 FUNLIST = [nerdemoji, blablabla]
 
 
-def generate_response(commentbody):
-    return choice(FUNLIST)(commentbody) + "\n\n^(I'm a bot and this action was performed automatically.)"
+def generate_response(commentbody, funlist):
+    return choice(funlist)(commentbody) + "\n\n^(I'm a bot and this action was performed automatically. See my pinned post for source code.)"
 
 
 selfbot = reddit.redditor("loserifybot")
 
+
+def has_reply_from_redditor(comment, redditor):
+    for reply in comment.replies:
+        if reply.author == redditor:
+            return True
+    return False
+
+
 while True:
     for subreddit in subreddits:
         for comment in subreddit.comments(limit=None):
-            if comment.score <= -10 and comment not in repliedcomments and comment.author != selfbot:
+            # comment if score is -10, not already been commented and it's not trying to reply to the bot itself.
+            if (comment.score <= -10) and (comment not in repliedcomments) and (not has_reply_from_redditor(comment, selfbot) and comment.author != selfbot):
                 try:
-                    comment.reply(generate_response(comment))
+                    comment.reply(generate_response(comment, FUNLIST))
                     repliedcomments.append(comment)
                     print(f"commented on " + comment.permalink)
                 except prawcore.exceptions.Forbidden:
